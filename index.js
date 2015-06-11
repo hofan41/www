03caa147b0ae3a2ef7ae2a5112e0a626b27e7ce1
hofan.kang <me@hofan41.com>
2015-06-11 10:56:43 -0700

    Added patent request

--- index.js
+++ index.js
@@ -29,16 +29,37 @@ server.views({
 });
 
 // Register any plugins
-server.route({
+server.route([{
 	path: '/',
 	method: 'GET',
 	handler: function(request, reply) {
 		return reply.view('index');
 	}
-});
+}, {
+	path: '/getPatents',
+	method: 'GET',
+	handler: function(request, reply) {
+		var Wreck = require('wreck');
+		var method = 'GET';
+		var uri = 'http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=0&f=S&l=50&TERM1=KANG%2C+HO-FAN&FIELD1=INNM&co1=AND&TERM2=&FIELD2=&d=PTXT';
 
-server.start(function(err){
-	if(err) {
+		Wreck.request(method, uri, null, function(err, response) {
+			if (err) {
+				reply(err);
+			} else {
+				Wreck.read(response, null, function(err, payload) {
+					if (err) {
+						reply(err);
+					}
+					reply(payload);
+				})
+			}
+		});
+	}
+}]);
+
+server.start(function(err) {
+	if (err) {
 		console.error(err);
 	} else {
 		console.log('server started!');
